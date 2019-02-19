c10bcf9be6203ac96694e6fece2e561d25ce372c
Peter Elliott <pelliott@ualberta.ca>
2019-02-18 20:23:55 -0700

    fixed no terminal echo

--- bootloader/stage2.c
+++ bootloader/stage2.c
@@ -14,7 +14,7 @@ static int *const printer_data   = (int *) 0177566;
 static int *const printer_status = (int *) 0177564;
 
 void write_term_ch(char ch) {
-    while (!(*printer_status) & 07) {}
+    while (!(*printer_status & 1<<7)) {}
     *printer_data = ch;
 }
 
@@ -22,7 +22,7 @@ static int *const reader_data   = (int *) 0177562;
 static int *const reader_status = (int *) 0177560;
 
 char read_term_ch(void) {
-    while (!(*reader_status & 07)) {}
+    while (!(*reader_status & 1<<7)) {}
     return *reader_data & 0377;
 }
 
